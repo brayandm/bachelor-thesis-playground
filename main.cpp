bc5ca95c7faf6e0b9aee50a366ce1b0c483422f4
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-01-31 14:58:21 +0100

    Add optimized compiler flags and include necessary libraries

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#pragma GCC optimize("O3", "Ofast", "no-stack-protector", "unroll-loops", "omit-frame-pointer", "inline")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#define debugMode true
+#include "/home/brayand/debugger.h"
+#else
+#define debugMode false
+#define db(...) false
+#define dbl(...) false
+#define dbg(...) false
+#define dbm(...) false
+#define dbs(...) false
+#define dbas(...) false
+#endif
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
+}
