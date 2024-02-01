bf926bf7b3eb746fbf4349b23b0f7daa5abc1c22
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-01 11:08:14 +0100

    Update .gitignore and run.sh scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ulimit -s unlimited && ./Build/Scheduler
+ulimit -s unlimited
+
+if [ $# -eq 0 ]; then
+    ./Build/Scheduler
+elif [ $# -eq 1 ]; then
+    ./Build/Scheduler "$1"
+else
+    ./Build/Scheduler "$1" "$2"
+fi
--- test.sh
+++ test.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+bash build.sh
+
+test_dir="Tests/"
+output_dir="Tests/Output/"
+
+mkdir -p "$test_dir"
+mkdir -p "$output_dir"
+
+for input_file in "$test_dir"*; do
+    if [ ! -f "$input_file" ]; then
+        continue
+    fi
+
+    filename=$(basename -- "$input_file")
+
+    output_file="${output_dir}${filename%.*}.out"
+
+    bash run.sh "$input_file" "$output_file"
+done
