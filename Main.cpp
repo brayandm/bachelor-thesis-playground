cfd8aeb51e887f36cdfb6858cae34132436fe2d9
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-01 10:53:18 +0100

    Fix scheduler initialization

--- Main.cpp
+++ Main.cpp
@@ -29,9 +29,9 @@ int32_t main(int argc, char *argv[])
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    // Scheduler scheduler;
+    Scheduler scheduler;
 
-    // scheduler.reader();
+    scheduler.reader();
 
     cout << "Hello World!\n";
 
