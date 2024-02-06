a43fcea542563ca4c9cc9253048db52e01c92c58
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-06 11:02:08 +0100

    Refactor experiment score input in save.sh

--- save.sh
+++ save.sh
@@ -4,8 +4,6 @@ read -p "Please enter the name of this experiment: " experiment_name
 
 read -p "Please enter the description of this experiment: " experiment_description
 
-read -p "Please enter the score in Codeforces of this experiment: " experiment_score
-
 experiment_name_snake_cased=$(echo $experiment_name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
 
 experiment_number=$(find Experiments -maxdepth 1 -type d | wc -l)
@@ -20,6 +18,5 @@ cp -r Classes/* "Experiments/$formatted_experiment_name/Classes/"
 
 echo "{
   \"name\": \"$experiment_name\",
-  \"description\": \"$experiment_description\",
-  \"codeforcesScore\": \"$experiment_score\"
+  \"description\": \"$experiment_description\"
 }" > "Experiments/${formatted_experiment_name}/Description.json"
