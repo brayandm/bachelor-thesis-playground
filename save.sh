570d2a656d4c359839a80190711cd557c0b4b068
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-06 11:00:33 +0100

    Add codeforces score to experiment description

--- save.sh
+++ save.sh
@@ -4,6 +4,8 @@ read -p "Please enter the name of this experiment: " experiment_name
 
 read -p "Please enter the description of this experiment: " experiment_description
 
+read -p "Please enter the score in Codeforces of this experiment: " experiment_score
+
 experiment_name_snake_cased=$(echo $experiment_name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
 
 experiment_number=$(find Experiments -maxdepth 1 -type d | wc -l)
@@ -18,5 +20,6 @@ cp -r Classes/* "Experiments/$formatted_experiment_name/Classes/"
 
 echo "{
   \"name\": \"$experiment_name\",
-  \"description\": \"$experiment_description\"
+  \"description\": \"$experiment_description\",
+  \"codeforcesScore\": \"$experiment_score\"
 }" > "Experiments/${formatted_experiment_name}/Description.json"
