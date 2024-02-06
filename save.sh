987c97f7721debb29c813b558a4018c506c9ff51
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-06 10:37:39 +0100

    Add experiment description and JSON file

--- save.sh
+++ save.sh
@@ -2,14 +2,23 @@
 
 read -p "Please enter the name of this experiment: " experiment_name
 
-experiment_name=$(echo $experiment_name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+read -p "Please enter the description of this experiment: " experiment_description
+
+experiment_name_snake_cased=$(echo $experiment_name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
 
 experiment_number=$(find Experiments -maxdepth 1 -type d | wc -l)
 
-formatted_experiment_name="exp${experiment_number}_${experiment_name}"
+formatted_experiment_name="exp${experiment_number}_${experiment_name_snake_cased}"
 
 mkdir -p "Experiments/$formatted_experiment_name/Classes"
 
 cp Main.cpp "Experiments/$formatted_experiment_name/Main.cpp"
 
 cp -r Classes/* "Experiments/$formatted_experiment_name/Classes/"
+
+touch "Experiments/$formatted_experiment_name/Description.txt"
+
+echo "{
+  \"name\": \"$experiment_name\",
+  \"description\": \"$experiment_description\",
+}" > "Experiments/${formatted_experiment_name}/Description.json"
