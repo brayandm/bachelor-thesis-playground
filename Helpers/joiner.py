678a47165731426a81d62cc638171cef56c0012e
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-21 18:14:06 +0100

    Refactor header file handling and add topological sorting for dependencies

--- Helpers/joiner.py
+++ Helpers/joiner.py
@@ -18,6 +18,35 @@ def remove_include_statements(file, include_statements):
     return file
 
 
+def find_header_dependencies(file, include_statement_header):
+
+    dependencies = set()
+
+    for include_statement in include_statement_header:
+        if include_statement in file:
+            dependencies.add(include_statement_header[include_statement])
+
+    return dependencies
+
+
+def topological_sort(dependency_dict):
+    visited = set()
+    topological_order = []
+
+    def dfs(node):
+        if node not in visited:
+            visited.add(node)
+            for neighbor in dependency_dict.get(node, []):
+                dfs(neighbor)
+            topological_order.insert(0, node)
+
+    for node in dependency_dict.keys():
+        if node not in visited:
+            dfs(node)
+
+    return topological_order[::-1]
+
+
 def main():
 
     directories = ["Classes/", "Algorithms/"]
@@ -25,6 +54,10 @@ def main():
     main_file = "Main.cpp"
 
     include_statements = []
+
+    include_statement_header = {}
+
+    dependencies = {}
 
     for directory in directories:
 
@@ -35,28 +68,47 @@ def main():
             include_statements.append(f'#include "{directory}{header_file}"')
             include_statements.append(f'#include "../{directory}{header_file}"')
 
-    combined_header_content = ""
-    combined_cpp_content = ""
+            include_statement_header[f'#include "{header_file}"'] = (
+                f"{directory}{header_file}"
+            )
+            include_statement_header[f'#include "{directory}{header_file}"'] = (
+                f"{directory}{header_file}"
+            )
+            include_statement_header[f'#include "../{directory}{header_file}"'] = (
+                f"{directory}{header_file}"
+            )
 
     for directory in directories:
 
         class_files = get_class_files(directory)
 
         for header_file, cpp_file in class_files:
-            header_content = extract_file_content(os.path.join(directory, header_file))
 
-            header_content = remove_include_statements(
-                header_content, include_statements
+            dependencies[f"{directory}{header_file}"] = find_header_dependencies(
+                extract_file_content(os.path.join(directory, header_file)),
+                include_statement_header,
             )
 
-            combined_header_content += header_content + "\n"
+    topological_order = topological_sort(dependencies)
 
-        for header_file, cpp_file in class_files:
-            cpp_content = extract_file_content(os.path.join(directory, cpp_file))
+    combined_header_content = ""
+    combined_cpp_content = ""
 
-            cpp_content = remove_include_statements(cpp_content, include_statements)
+    for header_file in topological_order:
 
-            combined_cpp_content += cpp_content + "\n"
+        cpp_file = header_file.replace(".h", ".cpp")
+
+        header_content = extract_file_content(header_file)
+
+        header_content = remove_include_statements(header_content, include_statements)
+
+        combined_header_content += header_content + "\n"
+
+        cpp_content = extract_file_content(cpp_file)
+
+        cpp_content = remove_include_statements(cpp_content, include_statements)
+
+        combined_cpp_content += cpp_content + "\n"
 
     main_content = extract_file_content(main_file)
 
