d302b821f477ff6dd91e7091ebf2f1cf1a4b5b45
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-21 11:46:06 +0100

    Add validation logic to Validator class

--- Classes/Validator.cpp
+++ Classes/Validator.cpp
@@ -1,1 +1,32 @@
 #include "Validator.h"
+
+void Validator::validateResult(DataStorage &dataStorage)
+{
+    for (int t = 0; t < dataStorage.input.T; t++)
+    {
+        for (int k = 0; k < dataStorage.input.K; k++)
+        {
+            double sumOverR = 0;
+
+            for (int r = 0; r < dataStorage.input.R; r++)
+            {
+                double sumOverN = 0;
+
+                for (int n = 0; n < dataStorage.input.N; n++)
+                {
+                    assert(dataStorage.output.p[k][r][n][t] >= 0);
+
+                    assert((dataStorage.output.p[k][r][n][t] == 0) ^ dataStorage.output.b[k][r][n][t]);
+
+                    sumOverN += dataStorage.output.p[k][r][n][t];
+                }
+
+                assert(sumOverN <= 4);
+
+                sumOverR += sumOverN;
+            }
+
+            assert(sumOverR <= dataStorage.input.R);
+        }
+    }
+}
--- Classes/Validator.h
+++ Classes/Validator.h
@@ -1,9 +1,13 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
+#include "DataStorage.h"
+#include <cassert>
+
 class Validator
 {
 public:
+    void validateResult(DataStorage &dataStorage);
 };
 
 #endif // VALIDATOR_H
