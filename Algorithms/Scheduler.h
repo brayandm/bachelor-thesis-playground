1643d4a05ffd80313a788cfecc78cb025096267b
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-21 13:27:30 +0100

    Remove unnecessary header includes in Scheduler.h

--- Algorithms/Scheduler.h
+++ Algorithms/Scheduler.h
@@ -1,10 +1,8 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-#include "../Classes/InputStorage.h"
 #include "../Classes/InputReader.h"
 #include "../Classes/DataStorage.h"
-#include "../Classes/OutputStorage.h"
 #include "../Classes/Precalculator.h"
 #include "../Classes/DataTransmissionCalculator.h"
 #include "../Classes/Printer.h"
