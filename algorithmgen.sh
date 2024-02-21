5f08cb1675017b7d26528c9a5af35e125f1f596c
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-21 19:37:03 +0100

    Add script to generate algorithm class files

---
+++ algorithmgen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p "Please enter the name of the algorithm: " algorithm_name
+
+mkdir -p Algorithms
+
+header_file="Algorithms/$algorithm_name.h"
+cpp_file="Algorithms/$algorithm_name.cpp"
+
+cat > "$header_file" <<EOF
+#ifndef ${algorithm_name^^}_H
+#define ${algorithm_name^^}_H
+
+class $algorithm_name
+{
+public:
+};
+
+#endif // ${algorithm_name^^}_H
+EOF
+
+cat > "$cpp_file" <<EOF
+#include "$algorithm_name.h"
+EOF
+
+echo "$algorithm_name class files have been created."
