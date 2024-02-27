16af07e150bdebce4efa84b400abef2a359676a7
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-27 19:01:39 +0100

    Refactor Scheduler::run() method

--- Algorithms/Scheduler.cpp
+++ Algorithms/Scheduler.cpp
@@ -22,111 +22,51 @@ void Scheduler::run()
         frameIds.push_back(std::get<2>(t));
     }
 
-    Scheduler::step(dataStorage, frameIds);
+    for (int i = 0; i < numberOfIterations; i++)
+    {
+        Scheduler::step(dataStorage, frameIds);
 
-    // for (int it = 0; it < numberOfIterations; it++)
-    // {
-    //     sort(frames.begin(), frames.end());
+        if (i == numberOfIterations - 1)
+            break;
 
-    //     std::vector<std::vector<std::pair<int, int>>> resourceBlockOcupation(dataStorage.input.T, std::vector<std::pair<int, int>>(dataStorage.input.R, {-1, -1}));
+        std::vector<double> powerPerFrame(dataStorage.input.J, 0);
 
-    //     std::vector<int> resourceBlockOcupationPerFrame(dataStorage.input.J, 0);
+        for (int j = 0; j < dataStorage.input.J; j++)
+        {
+            for (int t = dataStorage.input.firstTTI[j]; t < dataStorage.input.firstTTI[j] + dataStorage.input.amountTTIs[j]; t++)
+            {
+                for (int r = 0; r < dataStorage.input.R; r++)
+                {
+                    for (int k = 0; k < dataStorage.input.K; k++)
+                    {
+                        if (dataStorage.output.b[k][r][dataStorage.input.userId[j]][t] == true)
+                        {
+                            powerPerFrame[j] += dataStorage.output.p[k][r][dataStorage.input.userId[j]][t];
 
-    //     std::vector<std::vector<std::tuple<int, int, int>>> ocuppiedCellsPerFrame(dataStorage.input.J);
+                            dataStorage.output.b[k][r][dataStorage.input.userId[j]][t] = false;
+                            dataStorage.output.p[k][r][dataStorage.input.userId[j]][t] = 0;
+                        }
+                    }
+                }
+            }
+        }
 
-    //     for (auto frame : frames)
-    //     {
-    //         int j = std::get<2>(frame);
+        std::vector<std::tuple<double, int>> powerPerFrameWithId;
 
-    //         double g = 0;
+        for (int j = 0; j < dataStorage.input.J; j++)
+        {
+            powerPerFrameWithId.push_back({powerPerFrame[j], j});
+        }
 
-    //         std::vector<std::tuple<int, int, int>> ocuppiedCells;
+        sort(powerPerFrameWithId.begin(), powerPerFrameWithId.end());
 
-    //         for (int t = dataStorage.input.firstTTI[j]; t < dataStorage.input.firstTTI[j] + dataStorage.input.amountTTIs[j]; t++)
-    //         {
-    //             int bestRBG = -1;
-    //             int bestCell = -1;
-    //             double maxSINR0 = -1e9;
+        frameIds.clear();
 
-    //             for (int r = 0; r < dataStorage.input.R; r++)
-    //             {
-    //                 if (resourceBlockOcupation[t][r] != std::pair<int, int>(-1, -1))
-    //                     continue;
-
-    //                 for (int k = 0; k < dataStorage.input.K; k++)
-    //                 {
-    //                     if (dataStorage.input.s0[k][r][dataStorage.input.userId[j]][t] > maxSINR0)
-    //                     {
-    //                         maxSINR0 = dataStorage.input.s0[k][r][dataStorage.input.userId[j]][t];
-    //                         bestRBG = r;
-    //                         bestCell = k;
-    //                     }
-    //                 }
-    //             }
-
-    //             if (bestRBG != -1)
-    //             {
-    //                 resourceBlockOcupation[t][bestRBG] = {bestCell, dataStorage.input.userId[j]};
-    //                 ocuppiedCells.push_back({t, bestRBG, bestCell});
-    //                 dataStorage.output.p[bestCell][bestRBG][dataStorage.input.userId[j]][t] = 1;
-    //                 dataStorage.output.b[bestCell][bestRBG][dataStorage.input.userId[j]][t] = true;
-
-    //                 resourceBlockOcupationPerFrame[j]++;
-
-    //                 g += DataTransmissionCalculator::computeGforFrameWithoutInterferences(dataStorage, j, t);
-
-    //                 if (g > dataStorage.input.TBS[j] + EPS)
-    //                     break;
-    //             }
-    //         }
-
-    //         if (g < dataStorage.input.TBS[j] + EPS)
-    //         {
-    //             for (auto cell : ocuppiedCells)
-    //             {
-    //                 int t = std::get<0>(cell);
-    //                 int r = std::get<1>(cell);
-    //                 int k = std::get<2>(cell);
-
-    //                 resourceBlockOcupation[t][r] = {-1, -1};
-    //                 dataStorage.output.p[k][r][dataStorage.input.userId[j]][t] = 0;
-    //                 dataStorage.output.b[k][r][dataStorage.input.userId[j]][t] = false;
-
-    //                 resourceBlockOcupationPerFrame[j]--;
-    //             }
-
-    //             ocuppiedCells.clear();
-    //         }
-
-    //         ocuppiedCellsPerFrame[j] = ocuppiedCells;
-    //     }
-
-    //     if (it == numberOfIterations - 1)
-    //         break;
-
-    //     frames.clear();
-
-    //     for (int j = 0; j < dataStorage.input.J; j++)
-    //     {
-    //         frames.push_back({resourceBlockOcupationPerFrame[j], dataStorage.input.TBS[j], j});
-    //     }
-
-    //     for (int j = 0; j < dataStorage.input.J; j++)
-    //     {
-    //         for (auto cell : ocuppiedCellsPerFrame[j])
-    //         {
-    //             int t = std::get<0>(cell);
-    //             int r = std::get<1>(cell);
-    //             int k = std::get<2>(cell);
-
-    //             resourceBlockOcupation[t][r] = {-1, -1};
-    //             dataStorage.output.p[k][r][dataStorage.input.userId[j]][t] = 0;
-    //             dataStorage.output.b[k][r][dataStorage.input.userId[j]][t] = false;
-
-    //             resourceBlockOcupationPerFrame[j]--;
-    //         }
-    //     }
-    // }
+        for (auto t : powerPerFrameWithId)
+        {
+            frameIds.push_back(std::get<1>(t));
+        }
+    }
 
     Printer::printResult(dataStorage);
 }
@@ -174,8 +114,8 @@ void Scheduler::step(DataStorage &dataStorage, std::vector<int> frameIds)
 
                     for (int k = 0; k < dataStorage.input.K; k++)
                     {
-                        if (cellOcupation[t][k] == true)
-                            continue;
+                        // if (cellOcupation[t][k] == true)
+                        //     continue;
 
                         if (dataStorage.input.s0[k][r][n][t] > maxSINR0)
                         {
@@ -195,7 +135,7 @@ void Scheduler::step(DataStorage &dataStorage, std::vector<int> frameIds)
                 cellOcupation[bestTTI][bestCell] = true;
                 TTIOccupation[bestTTI] = true;
 
-                dataStorage.output.p[bestCell][bestRBG][n][bestTTI] = 4;
+                dataStorage.output.p[bestCell][bestRBG][n][bestTTI] = 1;
                 dataStorage.output.b[bestCell][bestRBG][n][bestTTI] = true;
                 options.push_back({bestTTI, bestRBG, bestCell});
 
