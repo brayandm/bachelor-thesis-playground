f4de64e8a4d93afeec6a69a556e7c42ae941c631
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-27 18:24:57 +0100

    Add build step and error handling

--- example.sh
+++ example.sh
@@ -7,4 +7,13 @@ if [ $# -gt 0 ]; then
     exit 1
 fi
 
+echo "Building..."
+
+error_output=$(bash build.sh 2>&1 > /dev/null)
+
+if [ $? -ne 0 ]; then 
+    echo -e "\033[31m$error_output\033[0m"
+    exit 1
+fi
+
 ./Build/Scheduler Tests/Example/example1
