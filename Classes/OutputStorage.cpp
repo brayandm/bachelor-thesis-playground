faf0231f0c65d0776e59074cf5c2968c022441f2
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-29 12:52:31 +0100

    Add clean() method to OutputStorage class

--- Classes/OutputStorage.cpp
+++ Classes/OutputStorage.cpp
@@ -1,1 +1,32 @@
 #include "OutputStorage.h"
+
+void OutputStorage::clean()
+{
+    for (int k = 0; k < p.size(); k++)
+    {
+        for (int r = 0; r < p[k].size(); r++)
+        {
+            for (int n = 0; n < p[k][r].size(); n++)
+            {
+                for (int t = 0; t < p[k][r][n].size(); t++)
+                {
+                    p[k][r][n][t] = 0;
+                }
+            }
+        }
+    }
+
+    for (int k = 0; k < b.size(); k++)
+    {
+        for (int r = 0; r < b[k].size(); r++)
+        {
+            for (int n = 0; n < b[k][r].size(); n++)
+            {
+                for (int t = 0; t < b[k][r][n].size(); t++)
+                {
+                    b[k][r][n][t] = false;
+                }
+            }
+        }
+    }
+}
--- Classes/OutputStorage.h
+++ Classes/OutputStorage.h
@@ -8,6 +8,8 @@ class OutputStorage
 public:
     std::vector<std::vector<std::vector<std::vector<double>>>> p;
     std::vector<std::vector<std::vector<std::vector<bool>>>> b;
+
+    void clean();
 };
 
 #endif // OUTPUTSTORAGE_H
