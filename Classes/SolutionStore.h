5e2bad5dfdbd09560d1eda2d2d52645d978be3fe
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-29 13:28:24 +0100

    Add SolutionStore class

---
+++ Classes/SolutionStore.h
@@ -0,0 +1,13 @@
+#ifndef SOLUTIONSTORE_H
+#define SOLUTIONSTORE_H
+
+#include "OutputStorage.h"
+
+class SolutionStore
+{
+public:
+    int dataFrameTransmitted;
+    OutputStorage output;
+};
+
+#endif // SOLUTIONSTORE_H
