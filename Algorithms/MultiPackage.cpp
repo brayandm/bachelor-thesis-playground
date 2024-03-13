e6fe9884719b1ee0b84112f03f9070eac9eee1fa
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-03-13 11:10:56 +0100

    Add run() method to MultiPackage class

--- Algorithms/MultiPackage.cpp
+++ Algorithms/MultiPackage.cpp
@@ -1,1 +1,23 @@
 #include "MultiPackage.h"
+
+void MultiPackage::run()
+{
+    DataStorage dataStorage;
+    InputReader::read(dataStorage.input);
+    dataStorage.fitSize();
+
+    SolutionManager solutionManager;
+
+    DeliveryCalculator deliveryCalculator(dataStorage);
+
+    SimpleGreedy::step(dataStorage, FrameSorter::getFramesSortByTTIAndTBS(dataStorage));
+    solutionManager.addSolution(dataStorage.output, deliveryCalculator.calculateDelivery(dataStorage));
+
+    SolutionStore bestSolution = solutionManager.getBestSolution();
+
+    Printer::printResult(bestSolution.output);
+}
+
+void MultiPackage::step(DataStorage &dataStorage, std::vector<int> frameIds)
+{
+}
--- Algorithms/MultiPackage.h
+++ Algorithms/MultiPackage.h
@@ -1,6 +1,18 @@
 #ifndef MULTIPACKAGE_H
 #define MULTIPACKAGE_H
 
+#include "../Classes/InputReader.h"
+#include "../Classes/DataStorage.h"
+#include "../Classes/Precalculator.h"
+#include "../Classes/DataTransmissionCalculator.h"
+#include "../Classes/Printer.h"
+#include "../Classes/FrameSorter.h"
+#include "../Classes/SolutionManager.h"
+#include "../Classes/SolutionStore.h"
+#include "../Classes/DeliveryCalculator.h"
+#include <vector>
+#include <algorithm>
+
 class MultiPackage
 {
 public:
